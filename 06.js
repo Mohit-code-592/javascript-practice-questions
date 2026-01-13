fe40d38399cc52aef927461f95891d4bd99d9f74
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2026-01-13 23:55:10 +0530

    Create 06.js

---
+++ 06.js
@@ -0,0 +1,8 @@
+//check a character is vowel or consonant 
+let char = 'g';
+if (char === 'i' || char === 'a' || char === 'u' || char === 'e' || char === 'o') {
+    console.log(`chat is vowel`);
+}
+else {
+    console.log(`char is consonant`);
+}
