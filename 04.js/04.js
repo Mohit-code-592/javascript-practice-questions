5d6f29959e456ff1f5e5dc7e6c069a26f6b56f8f
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2026-01-13 23:53:46 +0530

    Create 04.js

---
+++ 04.js/04.js
@@ -0,0 +1,8 @@
+// check year is leap year ?
+let year = 2002;
+if (year % 4 === 0 || (year % 4 === 0 && year % 100 === 0)) {
+    console.log(`${year} is a leap year`);
+}
+else {
+    console.log(`${year} is not a leap year`);
+}
