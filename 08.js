b56e5a2367f0b5a0e05cf1f5fb3628cdcf234c51
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2026-01-13 23:56:19 +0530

    Create 08.js

---
+++ 08.js
@@ -0,0 +1,15 @@
+//calculate grade based on marks
+let marks = 55;
+if (marks > 80 && marks <= 100) {
+    console.log("A");
+}
+else if (marks > 60 && marks <= 80) {
+    console.log("B");
+}
+else if (marks > 40 && marks <= 60) {
+    console.log("C");
+}
+else {
+    console.log("F");
+}
+
