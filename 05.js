6a1a75d957f1fa70af553e0c97ee3098a6793937
Mohit-code-592 <mr.mohitkumar592@gmail.com>
2026-01-13 23:54:32 +0530

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,8 @@
+//check whether a number is divisible by 5 and 11
+let number = 55;
+if (number % 5 === 0 && number % 11 === 0) {
+    console.log(`number is divisible by 5 and 11`);
+}
+else {
+    console.log(`number is not divisible by 5 and 11`);
+}
